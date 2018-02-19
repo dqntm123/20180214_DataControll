f80d26df9e52bd8423abdff74582648d18178061
dqntm123 <36214326+dqntm123@users.noreply.github.com>
2018-02-19 17:03:07 +0900

    20170219
    JSON

---
+++ Assets/Scripts/GetJsonData.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MiniJSON;
+
+public class GetJsonData : MonoBehaviour {
+
+    public TextAsset jsonData;
+    public Dictionary<string, object> unitData;
+    public GameObject unit;
+
+	void Start ()
+    {   //json은 MiniJSon스크립트에 선언된 함수다.
+        var dict = Json.Deserialize(jsonData.ToString()) as Dictionary<string, object>;//텍스트 에섯으로 선언한 jsondata를 투스트링으로 형변환하여 dictionary안에 있는 dict에다 집어넣는다.
+        List<object> unitInfo = dict["UnitInfo"] as List<object>;//list에다가  위에 dict안에 집어넣은 json을 넣는다.
+        for (int i = 0; i < unitInfo.Count; i++)
+        {
+            Dictionary<string, object> unitData = unitInfo[i] as Dictionary<string, object>;
+            GameObject unitObj = Instantiate(unit) as GameObject;
+            unitObj.GetComponent<UnitScript>().unitName =unitData["이름"].ToString();
+            unitObj.GetComponent<UnitScript>().unitHP = int.Parse(unitData["체력"].ToString());
+            unitObj.GetComponent<UnitScript>().unitMP = int.Parse(unitData["마나"].ToString());
+            unitObj.GetComponent<UnitScript>().unitATK = int.Parse(unitData["공격력"].ToString());
+            unitObj.GetComponent<UnitScript>().unitDEF = int.Parse(unitData["방어력"].ToString());
+        }
+	}
+}
---
+++ Assets/Scripts/GetUserData.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MiniJSON;
+using UnityEngine.UI;
+
+public class GetUserData : MonoBehaviour {
+
+    public TextAsset userData;
+    public Text[] userDT;
+    public float winSC = 0;
+    public float loseSC = 0;
+    public float shiftSC;
+	void Start ()
+    {
+        var dict = Json.Deserialize(userData.ToString()) as Dictionary<string, object>;
+        shiftSC = winSC / (winSC + loseSC) * 100;
+        int totalSC = (int)shiftSC;
+        //Debug.Log(dict["userid"]);
+        //Debug.Log(dict["username"]);
+        //Debug.Log(dict["win"]);
+        //Debug.Log(dict["lose"]);
+        //Debug.Log(dict["lastedConnect"]);
+        userDT[0].text = "유저 ID :" + dict["userid"].ToString();
+        userDT[1].text = "유저이름 :" + dict["username"].ToString();
+        userDT[2].text = "승리 :"+dict["win"].ToString();
+        userDT[3].text = "패배 : " + dict["lose"].ToString();
+        userDT[4].text = "마지막접속시간 :" + dict["lastedConnect"].ToString();
+        userDT[5].text ="승률 :"+ totalSC +"%".ToString();
+    }
+}
